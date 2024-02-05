18fe23df4a37648ac45e533ab6550a6021955ad9
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-02-05 17:12:07 +0100

    added themes

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const tiles = document.getElementsByClassName("tiles");
     const tilesCollection = [...tiles];
-    const iconCircle = "bxl-github";
-    const iconCross = "bxl-visual-studio";
+    const themeDisplay = document.querySelector(".start-game");
+
+    const themesCross = ["bxl-visual-studio", "bxl-bing", "bx-x-circle", "bxs-sun"];
+    const themesCircle = ["bxl-github", "bxl-opera", "bx-check-circle", "bxs-moon"];
+
+    let iconCross;
+    let iconCircle;
 
     const winPatterns = [
         ["a1", "a2", "a3"],
@@ -22,18 +27,60 @@ document.addEventListener("DOMContentLoaded", () => {
     let playerOtiles = [];
     let gameStatus = 0;
 
-    tilesCollection.forEach((tile) => {
-        tile.addEventListener("click", () => {
-            const tileID = tile.id;
-            handleClick(tileID);
-        });
-    });
+    handleDialog("theme");
+    displayThemes();
 
-    handleDialog("playerXturn");
+    function displayThemes() {
+        //afficher les thèmes
+        for (let i = 0; i < themesCross.length; i++) {
+            const div = document.createElement("div");
+            div.classList.add("themes");
+            div.id = `theme${i}`;
+            const cross = document.createElement("i");
+            cross.classList.add("bx", themesCross[i]);
+            const circle = document.createElement("i");
+            circle.classList.add("bx", themesCircle[i]);
+            div.appendChild(cross);
+            div.appendChild(circle);
+            div.addEventListener("click", () => {
+                handleTheme(themesCross[i], themesCircle[i]);
+            });
+            themeDisplay.appendChild(div);
+        }
+
+        //afficher le thème aléatoire
+        let random = Math.floor(Math.random() * themesCross.length);
+        const div = document.createElement("div");
+        div.classList.add("themes");
+        div.id = `theme-random`;
+        const cross = document.createElement("i");
+        cross.classList.add("bx", themesCross[random]);
+        const randomThemeCross = themesCross[random];
+
+        random = Math.floor(Math.random() * themesCross.length);
+        const circle = document.createElement("i");
+        circle.classList.add("bx", themesCircle[random]);
+        const randomThemeCircle = themesCircle[random];
+        div.appendChild(cross);
+        div.appendChild(circle);
+        div.addEventListener("click", () => {
+            handleTheme(randomThemeCross, randomThemeCircle);
+        });
+        themeDisplay.appendChild(div);
+    }
+
+    function handleTheme(cross, circle) {
+        iconCross = cross;
+        iconCircle = circle;
+        startGame();
+    }
 
     function handleDialog(status) {
         const dialog = document.getElementById("dialog");
         switch (status) {
+            case "theme":
+                dialog.textContent = "Choisissez votre thème.";
+                break;
             case "playerXturn":
                 dialog.textContent = "Tour du joueur X.";
                 break;
@@ -107,11 +154,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (X === 3) {
                     handleDialog("playerXwins");
                     gameStatus++;
-                    lockTiles();
+                    endGame();
                 } else if (O === 3) {
                     handleDialog("playerOwins");
                     gameStatus++;
-                    lockTiles();
+                    endGame();
                 }
             });
         });
@@ -119,11 +166,27 @@ document.addEventListener("DOMContentLoaded", () => {
         if (gameTurn >= 9 && gameStatus === 0) {
             handleDialog("draw");
             gameStatus++;
-            lockTiles();
+            endGame();
         }
     }
 
-    function lockTiles() {
+    function startGame() {
+        const container = document.querySelector(".container");
+        container.classList.remove("blur-mask");
+
+        themeDisplay.classList.add("hidden");
+
+        tilesCollection.forEach((tile) => {
+            tile.addEventListener("click", () => {
+                const tileID = tile.id;
+                handleClick(tileID);
+            });
+        });
+
+        handleDialog("playerXturn");
+    }
+
+    function endGame() {
         tilesCollection.forEach((tile) => {
             tile.classList.add("filled-tiles");
         });
@@ -133,7 +196,7 @@ document.addEventListener("DOMContentLoaded", () => {
         container.classList.add("blur-mask");
 
         // remove hidden from mask
-        const mask = document.querySelector(".mask");
+        const mask = document.querySelector(".end-game");
         mask.classList.remove("hidden");
 
         // add event listener to replay icon
